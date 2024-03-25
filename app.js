cca8a43ea8b889facd415e775a655727a7807821
reshad131 <161569107+reshad131@users.noreply.github.com>
2024-03-26 03:15:48 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const newArray = array1.concat(array2);
+// console.log(newArray); 
+
+// const str = "Hello ";
+// const repeatedStr = str.repeat(3);
+// console.log(repeatedStr);
+
+// const str = "Hello, World!";
+// const pattern = "Hello";
+// const result = str.match(pattern);
+// console.log(result);
+
+// const str = "Hello, World!";
+// const pattern = /Hello/;
+// const result = pattern.test(str);
+// console.log(result); 
+
+// const str = "Hello, World!";
+// const result = str.includes("Hello");
+// console.log(result); 
+
+
+
+
+
+// Task 1
+
+
+// let input = prompt("Ad:");
+// let uzunluq = input.length;
+// console.log("Ad " + uzunluq + " simvoldan ibarətdir");
+
+// Task 2
+
+// var input1 = prompt("Söz:");
+// var input2 = prompt("Simvol:");
+
+// var tekrar = input1.includes(input2);
+
+// console.log(tekrar);
+
+
+
+// Task 3
+
+
+// var input1 = prompt("1. Söz: ");
+// var input2 = prompt("2. Söz: ");
+
+// if (input1.startsWith(input2)) {
+//     console.log("Birinci söz ikinci daxil etdiyin simvolla başlayır");
+// } else {
+//     console.log("Birinci söz ikinci daxil etdiyin simvolla başlamır.");
+// }
+
+
+
+// Task 4
+
+// var input1 = prompt("1. Söz: ");
+// var input2 = prompt("2. Söz: ");
+
+// if (input1.endsWith(input2)) {
+//     console.log("Birinci söz ikinci daxil etdiyin simvolla bitir");
+// } else {
+//     console.log("Birinci söz ikinci daxil etdiyin simvolla bitmir");
+// }
+
+// Task 5
+
+// var input1 = prompt("Söz:");
+// var input2 = prompt("Simvol:");
+
+// var index = input1.indexOf(input2);
+// console.log(index)
+
+
+// Task 5 version 2
+
+// var input1 = prompt("Söz:");
+// var input2 = prompt("Simvol:");
+// if (index !== -1) {
+//     console.log("Sözün içindəki simvolun index: " + index);
+// } else {
+//     console.log("Sözün içindəki simvolun index-i yoxdur");
+// }
+
+
+// Task 6
+
+// var input = prompt("Lütfen bir söz giriniz:");
+
+// var boyukHerf = input === input.toUpperCase();
+
+// console.log(boyukHerf);
